4c0833a3284a3e0582f7015990df2c52c54012d9
ortizfram <ortizfranco48@gmail.com>
2023-07-20 15:34:26 -0300

    use static in layout to cahrge main.css

---
+++ tasks/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from .views import list_tasks
+
+urlpatterns = [path("", list_tasks)]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def list_tasks(request):
+    return render(request, "list_tasks.html")
+
+
+def home(request):
+    return render(request, "home.html")
